a75566fb41defb70206b15926871ce7a516a8972
Lebogang Mabala <lebogang@naked.insure>
2018-03-31 10:56:28 +0200

    Adds pieces to one file

---
+++ src/classes/Piece/Pieces.ts
@@ -0,0 +1,6 @@
+export {default as Pawn } from 'classes/Piece/Pawn';
+export {default as King } from 'classes/Piece/King';
+export {default as Queen } from 'classes/Piece/Queen';
+export {default as Knight } from 'classes/Piece/Knight';
+export {default as Rook } from 'classes/Piece/Rook';
+export {default as Bishop } from 'classes/Piece/Bishop';
